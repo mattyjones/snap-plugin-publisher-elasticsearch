c8c2bf215ca3d59f4f83790cc8bb06b2eff55aa7
rkh <rkhebb@gmail.com>
2017-01-10 22:33:06 -0500

    Initial support for bulk API formatting.  For some reason providing http.NewRequest with more than a 4kb body got the payload cut off so for now I wait until I see 2000 bytes and transmit a bulk update

--- elasticsearch/elasticsearch.go
+++ elasticsearch/elasticsearch.go
@@ -1,17 +1,24 @@
 package elasticsearch
 
 import (
-        "bytes"
-        "net/http"
-        "encoding/json"
-        "strings"
-        "time"
+  "bytes"
+  "net/http"
+  "encoding/json"
+  "strings"
+  "time"
+  "fmt"
+  "io/ioutil"
 
-        log "github.com/Sirupsen/logrus"
-        "github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
+  log "github.com/Sirupsen/logrus"
+  "github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
 )
 
 type EPublisher struct {
+}
+
+//NewElasticsearchPublisher returns an instance of the Elasticsearch publisher
+func NewElasticsearchPublisher() *EPublisher {
+        return &EPublisher{}
 }
 
 func (f EPublisher) GetConfigPolicy() (plugin.ConfigPolicy, error) {
@@ -21,25 +28,42 @@ func (f EPublisher) GetConfigPolicy() (plugin.ConfigPolicy, error) {
   return *policy, nil
 }
 
+type Index struct {
+  Index string `json:"_index"`
+  Type  string `json:"_type"`
+}
+
+type ESBulkMetadata struct {
+  Index Index `json:"index"`
+}
+
 type MetricToPublish struct {
-	// The timestamp from when the metric was created.
-	Key string
+  // The timestamp from when the metric was created.
+  Key string
   Value float64
   Timestamp string
 }
 
 // Publish Elasticsearch publish function
 func (f EPublisher) Publish(mts []plugin.Metric, cfg plugin.Config) error {
+  var sampleBuffer bytes.Buffer
+  var body []byte
   url, err := cfg.GetString("uri")
-	if err != nil {
+        if err != nil {
     log.Errorf("unable to parse elasticsearch uri from configs")
-		return err
+                return err
   }
-	var tagsForPrefix []string
-
+  i := ESBulkMetadata{
+     Index{
+        Index: "snap",
+        Type:    "default",
+    },
+  }
+  indexTargetStr, err := json.Marshal(i)
+        var tagsForPrefix []string
   tagConfigs, err := cfg.GetString("prefix_tags")
-  	if err == nil {
-  		tagsForPrefix = strings.Split(tagConfigs, ",")
+        if err == nil {
+                tagsForPrefix = strings.Split(tagConfigs, ",")
   }
   // log.Debug("Start elastic publish")
   for _, m := range mts {
@@ -51,17 +75,37 @@ func (f EPublisher) Publish(mts []plugin.Metric, cfg plugin.Config) error {
       }
     }
     s := MetricToPublish{key, m.Data.(float64), m.Timestamp.Format(time.RFC3339)}
-    // log.Debug(s)
-    jsonStr, err := json.Marshal(s)
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-    req.Header.Set("Content-Type", "application/json")
-    client := &http.Client{}
-    resp, err := client.Do(req)
+    MetricJSONStr, err := json.Marshal(s)
     if err != nil {
-        panic(err)
+      log.Errorf("Unable to marshal metric. Error: %s", err)
+      return fmt.Errorf("Unable to marshal metric. Error: %s", err)
     }
-    defer resp.Body.Close()
+    sampleBuffer.WriteString(string(indexTargetStr))
+    sampleBuffer.WriteString("\n")
+    //log.Errorf("Build! %i", len(sampleBuffer.String()))
+    sampleBuffer.WriteString(string(MetricJSONStr))
+    sampleBuffer.WriteString("\n")
+    if len(sampleBuffer.String()) > 2000 {
+      log.Errorf("%s", sampleBuffer.String())
+      req, err := http.NewRequest("POST", url, &sampleBuffer)
+      req.Header.Set("Content-Type", "application/json")
+      timeout := time.Duration(30 * time.Second)
+      client := http.Client{
+        Timeout: timeout,
+      }
+      resp, err := client.Do(req)
+      if err != nil {
+        log.Errorf("%s", err)
+      }
+      defer resp.Body.Close()
+      body, _ = ioutil.ReadAll(resp.Body)
+      log.Errorf("%s", string(body))
+      if err != nil {
+        log.Errorf("Unable to send metrics. Error: %s", err)
+        return fmt.Errorf("Unable to send metrics. Error: %s", err)
+      }
+    sampleBuffer.Reset()
+    }
   }
-
   return nil
 }
